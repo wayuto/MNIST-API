42f86839a22bc3b450e53abe6d197876ad719df9
wayuto <4e07910@gmail.com>
2025-07-24 12:43:46 +0800

    commit

--- mnist_api.py
+++ mnist_api.py
@@ -1,33 +1,52 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-import torchvision.transforms as transforms
-
-from random import random
 import os
 
-transform = transforms.Compose([
-    transforms.ToTensor(), 
-    transforms.Normalize((0.5,), (0.5,))
-])
-
 class NN(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, dropout_rate=0.4):
         super(NN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(64 * 7 * 7, 128)
-        self.fc2 = nn.Linear(128, 10)
+        
+        self.conv_block1 = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(negative_slope=0.01, inplace=True)
+        )
+        self.pool1 = nn.MaxPool2d(2)
+        
+        self.conv_block2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ELU(alpha=1.0, inplace=True)
+        )
+        self.pool2 = nn.MaxPool2d(2)
+        
+        self.fc_block = nn.Sequential(
+            nn.Linear(64 * 7 * 7, 512),
+            nn.BatchNorm1d(512),
+            nn.SiLU(inplace=True),
+            nn.Dropout(dropout_rate),
+            
+            nn.Linear(512, 256),
+            nn.BatchNorm1d(256),
+            nn.Mish(inplace=True),
+            nn.Dropout(dropout_rate * 0.7),
+            
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
+            nn.GELU(),
+            nn.Dropout(dropout_rate * 0.5),
+            
+            nn.Linear(128, 10)
+        )
 
-    def forward(self, x) -> torch.Tensor:
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2)
+    def forward(self, x):
+        x = self.conv_block1(x)
+        x = self.pool1(x)
+        x = self.conv_block2(x)
+        x = self.pool2(x)
         x = torch.flatten(x, 1)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = self.fc_block(x)
         return x
         
 device = torch.device("cpu")
