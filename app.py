02258d85cde9ef5ad7f3dafa0dbc79d33e3b9f48
wayuto <4e07910@gmail.com>
2025-07-24 11:22:20 +0800

    commit

--- app.py
+++ app.py
@@ -14,6 +14,6 @@ def home() -> str:
 
 if __name__ == '__main__':
     app.run(
-            host="0.0.0.0",
-            port=5000,
-            debug=True)
+            host='0.0.0.0', 
+            port=9000
+            )
